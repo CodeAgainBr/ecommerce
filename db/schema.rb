8c239bab9c2db633a1c50edb244cf60127343fa7
alexrms <ricdoa2@gmail.com>
2018-12-13 00:02:36 -0200

    Final commit

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 20181030221329) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
+    t.string   "nome"
+    t.string   "login"
+    t.date     "data_nascimento"
+    t.string   "cpf"
+    t.boolean  "admin",                  default: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -23,11 +28,6 @@ ActiveRecord::Schema.define(version: 20181030221329) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "nome"
-    t.string   "login"
-    t.date     "data_nascimento"
-    t.string   "cpf"
-    t.boolean  "admin",                  default: false
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
