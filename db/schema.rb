e92c51dd893b8e570c6e25b1c56e791908478ae5
GilmarBec <trabalhos4.5.1.4@gmail.com>
2018-12-14 21:09:54 -0200

    push leo

--- db/schema.rb
+++ db/schema.rb
@@ -32,11 +32,11 @@ ActiveRecord::Schema.define(version: 20181106203610) do
   end
 
   create_table "itens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "quantidade", default: "0"
+    t.integer  "quantidade", default: 0
     t.integer  "produto_id"
     t.integer  "pedido_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.index ["pedido_id"], name: "index_itens_on_pedido_id", using: :btree
     t.index ["produto_id"], name: "index_itens_on_produto_id", using: :btree
   end
